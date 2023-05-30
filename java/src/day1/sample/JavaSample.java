9b624897bc286bb7f685bd7ba3e9005236d0f72b
ekzot1212 <134986753+ekzot1212@users.noreply.github.com>
2023-05-30 12:45:43 +0900

    자바 프로젝트 생성 및 자바 샘플 코드 추가

---
+++ java/src/day1/sample/JavaSample.java
@@ -0,0 +1,17 @@
+package day1.sample;
+
+public class JavaSample {
+
+	public static void main(String[] args) {
+		
+		
+		/**
+		 * 메소드를 설명하는 주석
+		 */
+		
+		// main = 프로그램이 실행되기 위해 필요한 것(메소드)
+		System.out.println("Hello world");
+		
+	}
+
+}
