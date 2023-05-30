3e9a75855e6517b769a4a8e0425c2c028215cb20
ekzot1212 <134986753+ekzot1212@users.noreply.github.com>
2023-05-30 15:07:04 +0900

    자바 : 변수 기본형 및 선언 방법 예제

---
+++ java/src/day1/variable/CharExample1.java
@@ -0,0 +1,22 @@
+package day1.variable;
+
+public class CharExample1 {
+
+	public static void main(String[] args) {
+		// 변수타입 변수명;
+		char ch1;
+		// 변수타입 변수명 = 초기값;
+		char ch2 = ' ';
+		// 변수타입 변수명1,변수명2;
+		char ch3, ch4;
+		// 변수타입 변수명1 = 초기값1, 변수명2 = 초기값2;
+		// \u0000 16진수 = 16진수에 맞는 문자를 가져옴
+		char ch5 = '\u0041' , ch6 = 65 , ch7 = '\n' ;		//유니코드 65는 A 이다.		\n = 한줄띄우기 \t = 탭키 공백삽입		
+															//  \\ = \문자, \' = '문자 , \" = "문자
+		
+		System.out.println(ch5);
+		System.out.println(ch6);
+
+	}
+
+}
---
+++ java/src/day1/variable/IntExample.java
@@ -0,0 +1,23 @@
+package day1.variable;
+
+public class IntExample {
+
+	public static void main(String[] args) {
+		
+		int num2 = 11;
+		int num3 = 0b11;		//2진수
+		int num4 = 011;			//8진수
+		int num5 = 0x11;		//16진수
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(num5);
+		
+		//1234567890123은 int로 표현할 수 없는 큰 숫자.
+		long num6 = 1234567890123L;		//long, float = 리터럴 뒤에 식별자 적어야 함.
+		
+		
+		
+	}
+
+}
