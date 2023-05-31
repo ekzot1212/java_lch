d80171d124946430aa216eff2c9a2c834a184253
ekzot1212 <134986753+ekzot1212@users.noreply.github.com>
2023-05-31 12:41:26 +0900

    자바 변수 연습문제

---
+++ java/src/day2/variable/Practice.java
@@ -0,0 +1,17 @@
+package day2.variable;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		
+		// 문제1
+		char ch1 = 'A';
+		
+		// 문제2
+		int kor ;
+		int eng ;
+		int math ;
+		double avg ;
+	}
+
+}
