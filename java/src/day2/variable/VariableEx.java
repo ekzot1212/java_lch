69a76666213774f090e052e93442d96e87465f40
ekzot1212 <134986753+ekzot1212@users.noreply.github.com>
2023-05-31 12:41:44 +0900

    자바 변수 선언 예제

---
+++ java/src/day2/variable/VariableEx.java
@@ -0,0 +1,15 @@
+package day2.variable;
+
+public class VariableEx {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		double num2 = 3e-3; 	// 3/1000 == 0.003
+		double num3 = 1e3;		// 1 x 10^3 == 1000
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+	}
+
+}
