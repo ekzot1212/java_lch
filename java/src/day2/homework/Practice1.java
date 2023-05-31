63ecbbfe9b356ff3c832388a618c3461608b945e
ekzot1212 <134986753+ekzot1212@users.noreply.github.com>
2023-05-31 15:12:30 +0900

    자바 day2 숙제

---
+++ java/src/day2/homework/Practice1.java
@@ -0,0 +1,20 @@
+/**
+ * 콘솔에서 정수 2개와 문자(산술연산자)를 입력받아 출력하는 코드를 작성하세요.
+ * 예 : 
+ * 두 정수와 산술연산자를 입력하세요 (예 :1 + 2) :
+ * [입력] 1 + 2 [엔터]
+ * [출력] 1 + 2 
+ */
+
+package day2.homework;
+
+import java.util.Scanner;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		Scanner sc1 = new Scanner(System.in);
+
+	}
+
+}
---
+++ java/src/day2/homework/Practice2.java
@@ -0,0 +1,27 @@
+		/**
+		 * 두 정수를 입력받아 두 정수의 산술 연산 결과를 출력하는 코드를 작성하세요. 
+		 * 단, 2번째 숫자는 0이 아니여야 함.
+		 * 
+		 * 예: 
+		 * 두 정수를 입력하세요.
+		 * 1 2 [엔터] 
+		 * 1 + 2 = 3
+		 * 1 - 2 = -1
+		 * 1 * 2 = 2
+		 * 1 / 2 = 0.5
+		 */
+		
+package day2.homework;
+
+import java.util.Scanner;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+
+}
