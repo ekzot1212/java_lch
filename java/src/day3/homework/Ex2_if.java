cacd559df28d804ee73a41c917603c29873ede7e
ekzot1212 <134986753+ekzot1212@users.noreply.github.com>
2023-06-01 15:15:41 +0900

    day3 숙제 (조건연산자,IF)

--- java/src/day3/homework/Ex2_if.java
+++ java/src/day3/homework/Ex2_if.java
@@ -1,17 +1,29 @@
 package day3.homework;
 
+import java.util.Scanner;
+
 public class Ex2_if {
 
 	public static void main(String[] args) {
 		/**
-		 * 성별(M:남성,W:여성)를 입력받아 M이면 남성을, W이면 여성을 출력하는 코드를 작성하세요. if문 이용 (package :
-		 * day3.homework, file : Ex2_if) input gender : M Are you a women? man
-		 * 
-		 * input gender : W Are you a women? woman
-		 * 
+		 * 성별(M:남성,W:여성)를 입력받아 M이면 남성을, W이면 여성을 출력하는 코드를 작성하세요. if문 이용 (package :day3.homework, 
+		 * file : Ex2_if) 
+		 * input gender : M 
+		 * Are you a women? man
+		 * input gender : W 
+		 * Are you a women? woman
 		 */
+		System.out.println("input gender : ");
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
 		
+		if(str.equals("M")) {
+			System.out.println("Are you a woman? man");
+		}
 		
+		if(str.equals("W")) {
+			System.out.println("Are you a woman? woman");
+		}
 	}
 
 }
