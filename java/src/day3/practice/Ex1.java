537f811feabd033a91b83252f422cde1f1f39003
ekzot1212 <134986753+ekzot1212@users.noreply.github.com>
2023-06-01 13:37:43 +0900

    자바 day3 연습문제

---
+++ java/src/day3/practice/Ex1.java
@@ -0,0 +1,38 @@
+		/**
+		 * 성적을 입력받아 입력받은 성적이 60점 이상인지 확인하는 코드를 작성하세요.
+		 * 예:
+		 * input score :
+		 * 65
+		 * Do you pass 65 points? true
+		 * input score :
+		 * 45
+		 * Do you pass 45 points? false
+		 */
+
+package day3.practice;
+
+import java.util.Scanner;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("input score :");
+		
+		int score = sc.nextInt();
+	//	System.out.println("Do you pass " + score + " points? " + (score >= 60));
+		
+		//		삼항연산자 사용하기
+		System.out.println("Do you pass " + score + " points? " + ((score >= 60) ? "pass" : "fail"));
+		
+		//		boolean 사용하기
+		//boolean scoreTest = score >= 60;
+		//System.out.println("Do you pass " + score + " points? " + scoreTest);
+		
+		
+		sc.close();
+		
+	}
+
+}
