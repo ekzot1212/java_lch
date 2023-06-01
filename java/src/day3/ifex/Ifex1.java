e23eb3c847d4c0390a1da6e3980ed0d8919d9226
ekzot1212 <134986753+ekzot1212@users.noreply.github.com>
2023-06-01 14:56:42 +0900

    자바 IF문 예제

---
+++ java/src/day3/ifex/Ifex1.java
@@ -0,0 +1,19 @@
+package day3.ifex;
+
+public class Ifex1 {
+
+	public static void main(String[] args) {
+		// 정수 num가 0이면 0입니다 라고 출력하는 코드를 작성하세요.
+
+		int i = 0;
+
+		if (i == 0) {
+			System.out.println("0입니다.");
+		}
+		// 정수 num가 0이 아니면, "0이 아닙니다" 라고 출력하는 코드를 작성하세요.
+		if (i != 0) {
+			System.out.println("0이 아닙니다.");
+		}
+	}
+
+}
