c1d24e6e2392004ddd075fac691c9d3888f61d0c
ekzot1212 <134986753+ekzot1212@users.noreply.github.com>
2023-06-02 14:49:09 +0900

    homework ScoreAverageEx

--- java/src/day4/homework/ScoreAverageEx.java
+++ java/src/day4/homework/ScoreAverageEx.java
@@ -1,5 +1,7 @@
 package day4.homework;
 
+import java.util.Scanner;
+
 public class ScoreAverageEx {
 
 	public static void main(String[] args) {
@@ -9,6 +11,14 @@ public class ScoreAverageEx {
 		 * 
 		 */
 		
+		System.out.println("국어, 영어, 수학 성적을 순서대로 입력하시오.");
+		Scanner sc = new Scanner(System.in);
+		
+		int korea = sc.nextInt();
+		int eng = sc.nextInt();
+		int math = sc.nextInt();
+		double avg = ((double)korea + eng + math)/3 ;
+		System.out.println("성적 평균은 " + avg + " 입니다.");
 		
 	}
 
