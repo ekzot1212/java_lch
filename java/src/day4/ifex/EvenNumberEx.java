9361a8db1a5d5df0ceb9781a52e3bf3cc43e7034
ekzot1212 <134986753+ekzot1212@users.noreply.github.com>
2023-06-02 11:49:40 +0900

    if else 예제 (짝수 홀수)

---
+++ java/src/day4/ifex/EvenNumberEx.java
@@ -0,0 +1,20 @@
+package day4.ifex;
+
+import java.util.Scanner;
+
+public class EvenNumberEx {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자 입력 :");
+		int num = sc.nextInt();
+		if(num%2 == 0 ) {
+			System.out.println("짝수 입니다.");
+		}
+		else {
+			System.out.println("홀수 입니다.");
+		}
+	}
+
+}
