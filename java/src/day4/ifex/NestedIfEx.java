5bad67c06656cc1372c2db672d0c17cb6f64db55
ekzot1212 <134986753+ekzot1212@users.noreply.github.com>
2023-06-02 14:35:42 +0900

    If else 예제

---
+++ java/src/day4/ifex/NestedIfEx.java
@@ -0,0 +1,34 @@
+package day4.ifex;
+
+public class NestedIfEx {
+
+	public static void main(String[] args) {
+		/*
+		 * 2, 3, 6의 배수 판별 예제
+		 * (day4.practice패키지에 IfMultipleEx 문제)
+		 * 
+		 */
+		
+		int num = 12;
+		
+		if(num % 2 == 0) {
+			if(num % 6 == 0) {
+				System.out.println("6의 배수입니다.");
+			}
+			
+			else {
+				System.out.println("2의 배수입니다.");
+			}
+		
+		}
+		
+		else if(num % 3 == 0) {
+			System.out.println("3의 배수입니다.");
+		}
+		
+		else {
+			System.out.println("2, 3, 6의 배수가 아닙니다.");
+		}
+		
+	}
+}
