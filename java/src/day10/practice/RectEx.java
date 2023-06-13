9cccdbf58049da3ad21b05bba67b0b26e0230877
ekzot1212 <134986753+ekzot1212@users.noreply.github.com>
2023-06-13 14:56:21 +0900

    day10

--- java/src/day10/practice/RectEx.java
+++ java/src/day10/practice/RectEx.java
@@ -1,38 +1,143 @@
 package day10.practice;
 
-import java.util.Scanner;
-
 public class RectEx {
 
 	public static void main(String[] args) {
-		System.out.println("만드려고 하는 직사각형의 가로 세로를 입력하시오 :");
-		Scanner sc = new Scanner(System.in);
 		
-		int width = sc.nextInt();
-		int height = sc.nextInt();
+		Rect1 r = new Rect1(0,0,10,10);
+		r.print();
+		r.move(10, 10);
+		r.print();
+		r.resize(10,10,20,20);
+		r.print();
+		r.resize(0, 0, 30, 30);
+		r.print();
 		
-		Rect rect = new Rect(width,height);
-		rect.drawRect();
+		System.out.println("R2 start!");
 		
+		Rect2 r2 = new Rect2(new Point(0,0), new Point(10,10));
+		r2.print();
+		r2.move(10, 10);
+		r2.print();
+		r2.resize(10, 10, 20, 20);
+		r2.print();
+		r2.resize(0, 0, 30, 30);
+		r2.print();
 	}
+
 }
 
-class Rect {			//public class 붙이려면 파일명과 클래스 명이 같아야함.!!
-	private int width;		//같은 패키지에 같은 이름의 클래스는 못만듬!
-	private int height;
-
-	
-	public Rect(int width, int height) {
-		this.width = width;
-		this.height = height;
+class Rect1{
+	//멤버변수
+	/*
+	 * 왼쪽 위의 점 x좌표, 왼쪽 위의 점 y좌표
+	 * 오른쪽 아래의 점 x좌표, 오른쪽 아래 점 y좌표
+	 */
+	private int leftUpX,leftUpY;
+	private int rightDownX, rightDownY;
+	//생성자
+	public Rect1(int leftUpX, int leftUpY, int rightDownX, int rightDownY) {
+		super();
+		this.leftUpX = leftUpX;
+		this.leftUpY = leftUpY;
+		this.rightDownX = rightDownX;
+		this.rightDownY = rightDownY;
 	}
-
-	
-	public void drawRect() {
-		System.out.println("첫번째 좌표 : 0, 0");
-		System.out.println("두번째 좌표 : " + width + ", 0");
-		System.out.println("세번째 좌표 : " + width + ", " + height );
-		System.out.println("네번째 좌표 : " + "0" + ", " + height );
+	//메서드
+	/**
+	 * 사각형 정보를 출력하는 메서드
+	 * 매개변수 :
+	 * 반환타입 :
+	 * 메서드명 :
+	 */
+	public void print() {
+		System.out.println("===============");
+		System.out.println("LU point : " + leftUpX + ", " + leftUpY);
+		System.out.println("RD point : " + rightDownX + ", " + rightDownY);
+	}
+	/**사각형을 이동시키는 메서드
+	 * 매개변수 : 이동시킬 왼쪽 위의 점 => int x, int y
+	 * 리턴타입 :
+	 * 메서드명 : move
+	 */
+	public void move(int x, int y) {
+		int dx = leftUpX - x;//이동한 x거리
+		int dy = leftUpY - y; //이동한 y거리
+		leftUpX = x;
+		leftUpY = y;
+		rightDownX = rightDownX - dx;
+		rightDownY = rightDownY -dy;
+		
+	}
+	/**사각형의 크기를 변경하는 메서드
+	 * 매개변수 : 변경된 사각형의 왼쪽 위의 x,y 좌표와
+	 * 			가로 , 세로가 주어지면 사각형의 크기를 변경하는 메서드.
+	 * 			=> int x, int y, int w, int h
+	 * 리턴타입 : 없음
+	 * 메서드명 : resize
+	 */
+	public void resize(int x, int y, int w, int h) {
+		leftUpX = x;
+		leftUpY = y;
+		rightDownX = x + w;
+		rightDownY = y + h;
 	}
 }
+////////////////////////////////////////////////////////////
+
+
+
+class Rect2 {
+	private Point leftUp, rightDown;
+
+	public Rect2(Point leftUp, Point rightDown) {
+		super();
+		this.leftUp = leftUp;
+		this.rightDown = rightDown;
+	}
+
+	public Rect2(int leftUpX, int leftUpY, int rightDownX, int rightDownY) {
+		leftUp = new Point(leftUpX, leftUpY);
+		rightDown = new Point(rightDownX, rightDownY);
+	}
+	
+	
+	//메서드
+		/**
+		 * 사각형 정보를 출력하는 메서드 매개변수 : 반환타입 : 메서드명 :
+		 */
+		public void print() {
+			System.out.println("===============");
+			System.out.println("lu point");
+			leftUp.print();
+			System.out.println("re point");
+			rightDown.print();
+		}
+
+		/**
+		 * 사각형을 이동시키는 메서드 매개변수 : 이동시킬 왼쪽 위의 점 => int x, int y 리턴타입 : 메서드명 : move
+		 */
+		public void move(int x, int y) {
+			int dx = leftUp.getX() - x;//이동한 x거리
+			int dy = leftUp.getY() - y; //이동한 y거리
+			leftUp.move(x,y);
+			rightDown.move(rightDown.getX() - dx,  rightDown.getY() -dy);
+			
+		}
+		/**사각형의 크기를 변경하는 메서드
+		 * 매개변수 : 변경된 사각형의 왼쪽 위의 x,y 좌표와
+		 * 			가로 , 세로가 주어지면 사각형의 크기를 변경하는 메서드.
+		 * 			=> int x, int y, int w, int h
+		 * 리턴타입 : 없음
+		 * 메서드명 : resize
+		 */
+		public void resize(int x, int y, int w, int h) {
+			leftUp.move(x,y);
+			rightDown.move(x+w,  y+h);
+		}
+		
+		
+}
+
+
 
