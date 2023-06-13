15545ed03a6cb747e46699383c968264d96beb59
ekzot1212 <134986753+ekzot1212@users.noreply.github.com>
2023-06-13 15:18:30 +0900

    day10

---
+++ java/src/day10/practice/PointEx2.java
@@ -0,0 +1,44 @@
+package day10.practice;
+
+public class PointEx2 {
+
+	public static void main(String[] args) {
+		/*
+		 * 좌표의 점을 관리하는 클래스를 만들고 테스트하는 코드를 작성하세요. 필요한 속성과 기능을 생각해서 추가해보세요. 
+		 * (예 : 그림판 하단의 좌표)
+		 */
+
+	
+	}
+
+}
+
+class Point2 {			//public class 붙이려면 파일명과 클래스 명이 같아야함.!!
+	private int x;		//같은 패키지에 같은 이름의 클래스는 못만듬!
+	private int y;
+	
+	public Point2(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void move(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	
+	
+	
+	
+}
+
---
+++ java/src/day10/practice/RectEx2.java
@@ -0,0 +1,44 @@
+package day10.practice;
+
+public class RectEx2 {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
+
+class Rect3{
+	
+	private Point2 LUP,RDP;
+	
+	
+	public Rect3(Point2 LUP, Point2 RDP) {
+		this.LUP = LUP;
+		this.RDP = RDP;
+	}
+	
+	
+	//메서드
+			/**
+			 * 사각형 정보를 출력하는 메서드 매개변수 : 반환타입 : 메서드명 :
+			 */
+			
+			/**
+			 * 사각형을 이동시키는 메서드 매개변수 : 이동시킬 왼쪽 위의 점 => int x, int y 리턴타입 : 메서드명 : move
+			 */
+			
+			/**사각형의 크기를 변경하는 메서드
+			 * 매개변수 : 변경된 사각형의 왼쪽 위의 x,y 좌표와
+			 * 			가로 , 세로가 주어지면 사각형의 크기를 변경하는 메서드.
+			 * 			=> int x, int y, int w, int h
+			 * 리턴타입 : 없음
+			 * 메서드명 : resize
+			 */
+			
+	
+}
+	
+
+
