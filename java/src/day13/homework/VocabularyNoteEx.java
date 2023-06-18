e5f0023ea3cd61a8a63e769d87dcb495a8365a1b
ekzot1212 <134986753+ekzot1212@users.noreply.github.com>
2023-06-19 01:02:52 +0900

    0619

--- java/src/day13/homework/VocabularyNoteEx.java
+++ java/src/day13/homework/VocabularyNoteEx.java
@@ -46,8 +46,11 @@ public class VocabularyNoteEx {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		char ch;
-		Word [] wordList = new Word[20];
+		char ch=0;
+		Word [] wordList = {
+			new Word("a", "a"),
+			new Word("b", "b")
+		};
 		VocabularyNote note = new VocabularyNote(wordList);
 		
 		do {
@@ -68,33 +71,57 @@ public class VocabularyNoteEx {
 			ch = sc.next().charAt(0);
 			if(ch == '1') {
 				System.out.println("추가할 단어와 뜻을 입력하시오.");
-				note.insert(sc.nextLine(),sc.nextLine());
+				note.insert(sc.next(),sc.next());
 				System.out.println("추가 성공.");
 			}
 			else if(ch == '2') {
 				System.out.println("삭제할 단어를 입력하시오.");
-				note.delete(sc.nextLine());
+				note.delete(sc.next());
 				System.out.println("삭제 성공.");
 			}
 			else if(ch == '3') {
-				System.out.println("    a. 단어 추가");
+				System.out.println("    a. 단어 수정");
 				System.out.println("    b. 뜻 수정");
 				System.out.println("    c. 뜻 삭제");
 				char ch2 = sc.next().charAt(0);
 				if(ch2 == 'a') {
 					System.out.println("수정할 단어를 입력하시오.");
+					note.updateTitle(sc.next(), sc.next());
+					System.out.println("수정 성공.");
 				}
+				
 				else if(ch2 == 'b') {
-					System.out.println("수정할 단어를 입력하시오.");
+					System.out.println("수정 할 단어, 번호, 수정할 뜻을 입력하시오.");
+					note.updateMeaning(sc.next(), sc.nextInt(), sc.next());
+					System.out.println("수정 성공.");
 				}
 				else if(ch2 == 'c') {
-					System.out.println("수정할 단어를 입력하시오.");
+					System.out.println("삭제 할 단어를 입력하시오.");
+					note.delete(sc.next());
+					System.out.println("삭제 성공.");
 				}
 				else {
 					System.out.println("메뉴 선택을 잘못하였습니다.");
 				}
 			}
-		}while(ch != 5);
+			else if(ch == '4') {
+				System.out.println("    a. 전체 출력");
+				System.out.println("    b. 검색");
+				char ch2 = sc.next().charAt(0);
+				if(ch2 == 'a') {
+					note.print();
+				}
+				
+				else if(ch2 == 'b') {
+					System.out.println("검색 할 단어를 입력하시오.");
+					note.search(sc.next());
+				}
+				
+				else {
+					System.out.println("메뉴 선택을 잘못하였습니다.");
+				}
+			}
+		}while(ch != '5');
 	}
 
 }
