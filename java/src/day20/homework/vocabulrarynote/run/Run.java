4ec88f0add2f7ff177af45b58907302e3d98cc5b
ekzot1212 <134986753+ekzot1212@users.noreply.github.com>
2023-06-27 15:10:51 +0900

    day20 homework

---
+++ java/src/day20/homework/vocabulrarynote/run/Run.java
@@ -0,0 +1,12 @@
+package day20.homework.vocabulrarynote.run;
+
+import day20.homework.vocabulrarynote.controller.VocabularyNoteController;
+
+public class Run {
+
+	public static void main(String[] args) {
+		VocabularyNoteController controller = new VocabularyNoteController();
+		controller.run();
+	}
+
+}
