59d27e3e369da12d1774b9d624433091e26e514e
han323012 <134986656+han323012@users.noreply.github.com>
2023-06-30 11:26:12 +0900

    Day 22 homework

--- java/src/day22/practice/phone2/run/PhoneListEx.java
+++ java/src/day22/practice/phone2/run/PhoneListEx.java
@@ -1,15 +1,30 @@
 package day22.practice.phone2.run;
 
-import day22.practice.phone2.vo.PhoneBook;
+import day22.practice.phone2.controller.PhoneManager;
 
 public class PhoneListEx {
 
 	public static void main(String[] args) {
+		
+		PhoneManager pm = new PhoneManager();
+		pm.run();
+		
+		
+		/*
 		PhoneBook pb = new PhoneBook();
 		System.out.println(pb.insertPhone("Hong", "010-1234-5678"));
 		System.out.println(pb.insertPhone("Lim", "010-123-456"));
 		pb.print(p->true);
 		
+		System.out.println(pb.updateName("Hong", "Hong gil"));
+		pb.print(p->true);
+		System.out.println(pb.updateNumber("Hong gil", "010-1111-5555"));	//
+		pb.print(p->true);
+		System.out.println(pb.update("Hong gil", "Lim", "010-1111-6666"));
+		pb.print(p->true);
+		System.out.println(pb.delete("Lim"));
+		pb.print(p->true);
+		*/
 	}
 
 }
