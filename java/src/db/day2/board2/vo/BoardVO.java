36b01b50c7417af98624b8bef8adb515562a375f
ekzot1212 <134986753+ekzot1212@users.noreply.github.com>
2023-08-20 20:29:14 +0900

    0818

---
+++ java/src/db/day2/board2/vo/BoardVO.java
@@ -0,0 +1,11 @@
+package db.day2.board2.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class BoardVO {
+	private String bo_title;
+	private String bo_me_id;
+}
