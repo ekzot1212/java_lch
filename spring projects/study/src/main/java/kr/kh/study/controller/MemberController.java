6bbb7881ca68429fbbc7cab941babd20515c75c3
ekzot1212 <ekzot1212@gmail.com>
2023-08-30 12:13:24 +0900

    SPRING : 복습 화면 구현, 회원가입까지

--- spring projects/study/src/main/java/kr/kh/study/controller/MemberController.java
+++ spring projects/study/src/main/java/kr/kh/study/controller/MemberController.java
@@ -2,9 +2,12 @@ package kr.kh.study.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import kr.kh.study.service.MemberService;
+import kr.kh.study.vo.MemberVO;
 
 //@Controller 어노테이션으로 컨트롤러로 인식되게 함
 @Controller
@@ -13,11 +16,27 @@ public class MemberController {
 	@Autowired
 	private MemberService memberService;
 	
-	@GetMapping("/test")
-	public String test() {
-		int count = memberService.count();
-		System.out.println(count);
-		// redirect : url을 메인으로 보내줌
-		return "redirect:/";
+	// signup = String을 반환하는 메서드
+	@GetMapping("/member/signup")
+	public String memberSignup() {
+		return "member/signup";
+	}
+	
+	//PostMapping으로 사용자가 입력한 member객체 받아옴
+	@PostMapping("/member/signup") //@RequestMapping(value="/member/signup", method=RequestMethod.POST)
+	public String memberSignupPost(Model model, MemberVO member) {
+		String msg, url;
+		
+		if(memberService.signup(member)) {
+			msg = "회원가입 성공!";
+			url = "/";
+		}else {
+			msg ="회원가입 실패!";
+			url ="/member/signup";
+		}
+		// model을 이용해서 jsp로 보낼수 있음. addAttribute(jsp의name, 보낼변수)
+		model.addAttribute("url", url);
+		model.addAttribute("msg", msg);
+		return "util/message";
 	}
 }
--- spring projects/study/src/main/webapp/WEB-INF/views/member/signup.jsp
+++ spring projects/study/src/main/webapp/WEB-INF/views/member/signup.jsp
@@ -8,11 +8,16 @@
 	 <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js"></script>
 	 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
 	 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-	<title>Home</title>
+	<title>회원가입</title>
 </head>
 <body>
-  
-      <a href="<c:url value='/member/signup'/>">회원가입</a>
-    
+	<h1>회원가입</h1>
+	<form action="<c:url value='/member/signup'/>" method="post">
+		<input type="text" name="me_id" placeholder="아이디" required> <br>
+		<input type="password" name="me_pw" placeholder="비번" required> <br>
+		<input type="text" name="me_email" placeholder="이메일" required> <br>
+		<button>회원가입</button>
+	</form>
+
 </body>
 </html>
