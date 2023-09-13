7e55d0152aa2e25079e80e0757d1beaff8fd4b9c
ekzot1212 <ekzot1212@gmail.com>
2023-09-13 17:12:41 +0900

    화면과 서버 사이의 데이터 전송 연습(2)
    누락본

---
+++ spring projects/springtest/src/main/java/kr/kh/springtest/HomeController.java
@@ -0,0 +1,49 @@
+package kr.kh.springtest;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import kr.kh.springtest.vo.MemberListVO;
+import kr.kh.springtest.vo.MemberVO;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model) {
+		model.addAttribute("num", 1);
+		return "home";
+	}
+	@GetMapping("/member/login")
+	public String memberLogin(String id, String pw) {
+		// 리턴값 void로 하면 알아서 jsp경로 찾아줌
+		System.out.println("ID : " + id);
+		System.out.println("PW : " + pw);
+		return "redirect:/";
+	}
+	@GetMapping("/member/login2")
+	public String memberLogin2(MemberVO member) {
+		System.out.println(member);
+		return "redirect:/";
+	}
+	@GetMapping("/member/login3")
+	public String memberLogin3(String []id, String []pw) {
+		for(String tmp : id) {
+			System.out.println("ID : " + tmp);
+		}
+		for(String tmp : pw) {
+			System.out.println("PW : " + tmp);
+		}
+		return "redirect:/";
+	}
+	@GetMapping("/member/login4")
+	public String memberLogin4(MemberListVO list) {
+		System.out.println(list);
+		return "redirect:/";
+	}
+}
