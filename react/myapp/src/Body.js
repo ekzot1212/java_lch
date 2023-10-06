90564a3a2d5a930503aae36433fe35a8176c2942
ekzot1212 <ekzot1212@gmail.com>
2023-10-06 12:53:42 +0900

    리액트 : 3일차,(복습, 크리에이트앱)

---
+++ react/myapp/src/Body.js
@@ -0,0 +1,19 @@
+import './css/App.css';
+
+function Body(){
+
+	return(
+		<div className="body">
+			<div className="container">
+				<div className="box">
+					<button>이전</button>	
+					<span>1 / 5</span>
+					<button>다음</button>	
+				</div>
+				<div className="box">2</div>
+			</div>
+		</div>
+	)
+}
+
+export default Body;
---
+++ react/myapp/src/Footer.js
@@ -0,0 +1,11 @@
+import './css/App.css'
+
+function Footer(){
+  return(
+    <div className='footer'>
+      <h1>footer</h1>
+    </div>
+  )
+}
+
+export default Footer;
